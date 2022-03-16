1d2072da8772cf21f9894cd775113cdea5248a04
aerialslash <33915211+aerialslash0422@users.noreply.github.com>
2022-03-16 18:22:55 +0900

    init

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const NETWORK_ID = "1001";
+const GASLIMIT = "20000000";
+const URL = `https://api.baobab.klaytn.net:8651`;
+const PRIVATE_KEY = "0x7f7937639237bea338732fb5d31cbb587156f87bcd2152bd2cf6bc3a80b5a78e";
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+
+    klaytn: {
+      provider: new HDWalletProvider(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    },
+  },
+};
